7b095dd42a08c433f2b3bd3e0f32dd7af8510d3f
Jakob Durstberger <jakob@durstberger.me>
2024-01-25 09:39:36 +0100

    Fixes tests

--- sub-project/test/index.test.ts
+++ sub-project/test/index.test.ts
@@ -3,6 +3,6 @@ import {describe, test, expect} from "vitest";
 
 describe('my add', () => {
   test('returns 3  for 1 + 2', () => {
-    expect(myAdd(1, 2)).toBe(4)
+    expect(myAdd(1, 2)).toBe(3)
   })
 })
