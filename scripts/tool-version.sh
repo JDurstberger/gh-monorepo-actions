f1ccc5984f32a97b3056d90140437890d5b9b4b5
Jakob Durstberger <jakob@durstberger.me>
2024-01-25 11:15:00 +0100

    JUST LINE

--- scripts/tool-version.sh
+++ scripts/tool-version.sh
@@ -6,5 +6,5 @@ PROJECT_DIR=$1
 TOOL=$2
 
 LINE="$(grep $TOOL "${PROJECT_DIR}/.tool-versions")"
-VERSION="$(grep -oEi '\d+.\d+.\d+' <<< "$LINE")"
-echo $VERSION
+#VERSION="$(grep -oEi '\d+.\d+.\d+' <<< "$LINE")"
+echo $LINE
